6074b5325d3dbef486fec80eb995ef971b828c26
Tony Hawkes <tonyhawkes@gmail.com>
2017-09-29 16:55:17 +0100

    Fix for broken cardboard button

--- android/src/main/java/com/xebia/googlevrpanorama/RNGoogleVRPanoramaView.java
+++ android/src/main/java/com/xebia/googlevrpanorama/RNGoogleVRPanoramaView.java
@@ -126,9 +126,9 @@ public class RNGoogleVRPanoramaView extends RelativeLayout {
     public void onAfterUpdateTransaction() {
         panoWidgetView = new VrPanoramaView(_activity);
         panoWidgetView.setEventListener(new ActivityEventListener());
-        panoWidgetView.setEnableCardboardButton(showStereo);
-        panoWidgetView.setEnableInfoButton(showInfo);
-        panoWidgetView.setEnableFullscreenButton(showFullScreen);
+        panoWidgetView.enableCardboardButton(showStereo);
+        panoWidgetView.enableInfoButton(showInfo);
+        panoWidgetView.enableFullscreenButton(showFullScreen);
         this.addView(panoWidgetView);
 
         if (imageLoaderTask != null) {
