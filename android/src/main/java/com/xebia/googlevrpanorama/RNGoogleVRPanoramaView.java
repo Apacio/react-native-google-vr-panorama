e470ac8f7dcd08e79c0586aeac016cc5d2a811f1
Tony Hawkes <tonyhawkes@gmail.com>
2017-10-01 20:50:53 +0100

    Testing easier local image use

--- android/src/main/java/com/xebia/googlevrpanorama/RNGoogleVRPanoramaView.java
+++ android/src/main/java/com/xebia/googlevrpanorama/RNGoogleVRPanoramaView.java
@@ -10,6 +10,7 @@ import android.util.Log;
 import android.util.Pair;
 import android.util.LruCache;
 import android.widget.RelativeLayout;
+import android.content.res.AssetManager;
 
 import com.facebook.react.bridge.Arguments;
 import com.facebook.react.bridge.ReactContext;
