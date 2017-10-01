fd7554281234d53a85530d584243f08900532aef
Tony Hawkes <tonyhawkes@gmail.com>
2017-10-01 21:24:08 +0100

    Testing easier local image use

--- android/src/main/java/com/xebia/googlevrpanorama/RNGoogleVRPanoramaView.java
+++ android/src/main/java/com/xebia/googlevrpanorama/RNGoogleVRPanoramaView.java
@@ -198,7 +198,7 @@ public class RNGoogleVRPanoramaView extends RelativeLayout {
 						}
 					}
 				} else {
-				    AssetManager assetManager = this.getReactApplicationContext().getAssets();
+				    AssetManager assetManager = getReactApplicationContext().getAssets();
 					imgFile = assetManager.open(url);
 
 					Log.d(TAG, "Image doesn't exist: " + url);
